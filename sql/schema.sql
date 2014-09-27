a57df1d7cca461f59e0e74635af4fa359c0d4af8
chrone <irie-yuki@kayac.com>
2014-09-27 14:01:54 +0900

    fix schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `login` varchar(255) NOT NULL UNIQUE,
   `password_hash` varchar(255) NOT NULL,
   `salt` varchar(255) NOT NULL,
-  fail_count int unsigned not null default 0
+  fail_count int unsigned NOT NULL DEFAULT 0
 ) DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `login_log` (
@@ -15,8 +15,17 @@ CREATE TABLE IF NOT EXISTS `login_log` (
   `succeeded` tinyint NOT NULL
 ) DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS ips (
+CREATE TABLE IF NOT EXISTS `ips` (
+  ip VARCHAR(255) NOT NULL,
+  fail_count INT UNSIGNED NOT NULL DEFAULT 0,
+  PRIMARY KEY (ip)
+) DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS success_log (
+  id int unsigned not null auto_increment,
+  user_id int not null,
   ip varchar(255) not null,
-  fail_count int unsigned not null default 0,
-  primary key (ip)
+  created_at timestamp not null default current_timestamp,
+  primary key (id),
+  index (user_id, created_at desc)
 ) DEFAULT CHARSET=utf8;
