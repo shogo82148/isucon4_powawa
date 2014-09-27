138678c597fd45fa73325f29c9395d41d0b0c52c
Ichinose Shogo <shogo82148@gmail.com>
2014-09-27 15:51:20 +0900

    インデックス貼ってみる

--- sql/schema.sql
+++ sql/schema.sql
@@ -12,7 +12,8 @@ CREATE TABLE IF NOT EXISTS `login_log` (
   `user_id` int,
   `login` varchar(255) NOT NULL,
   `ip` varchar(255) NOT NULL,
-  `succeeded` tinyint NOT NULL
+  `succeeded` tinyint NOT NULL,
+  index (user_id)
 ) DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS ips (
